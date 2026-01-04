44e8c75effbf3c1fab640df863356976682492e7
Yuriy Hrechka <yuriy.grechka.vitaliyovich@gmail.com>
2026-01-04 17:58:42 +0200

    feat: add settings.py & gitignore

---
+++ app/core/settings.py
@@ -0,0 +1,45 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+from pydantic import computed_field
+
+
+class Settings(BaseSettings):
+    model_config = SettingsConfigDict(
+        env_file=".env", env_ignore_empty=True, extra="ignore"
+    )
+
+    API_PREFIX: str = "/api/v1"
+    PROJECT_NAME: str = "Messenger"
+
+    SECRET_KEY: str = "your-secret-key"
+    ALGORITHM: str = "HS256"
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
+
+    POSTGRES_USER: str
+    POSTGRES_PASSWORD: str = ""
+    POSTGRES_HOST: str
+    POSTGRES_PORT: int
+    POSTGRES_DB: str
+
+    @computed_field
+    @property
+    def DATABASE_URL(self) -> str:
+        return (
+            f"postgresql+asyncpg://{self.POSTGRES_USER}:{self.POSTGRES_PASSWORD}@"
+            f"{self.POSTGRES_HOST}:{self.POSTGRES_PORT}/{self.POSTGRES_DB}"
+        )
+
+    REDIS_HOST: str
+    REDIS_PORT: int = 6379
+
+    @computed_field
+    @property
+    def REDIS_URL(self) -> str:
+        return f"redis://{self.REDIS_HOST}:{self.REDIS_PORT}/0"
+
+    MINIO_ROOT_USER: str
+    MINIO_ROOT_PASSWORD: str
+    MINIO_URL: str
+    MINIO_BUCKET_NAME: str = "messenger-media"
+
+
+settings = Settings()  # type: ignore
